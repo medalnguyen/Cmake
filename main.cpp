9191bc2127f528b5f2abe8443c03aa7ebbdaaba5
Nguyễn Huy Chương <74437696+medalnguyen@users.noreply.github.com>
2021-09-10 19:32:16 +0700

    Thu file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
-#include "Adder/adder.h"
+#include <adder.h>
+#include "GLFW/glfw3.h"
 
 int main(){
     std::cout << "Hello World" << std::endl;
     std::cout << add(5.4f,5.6f) << std::endl;
+
+    GLFWwindow* window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit( EXIT_SUCCESS );
     return 0;
 }
