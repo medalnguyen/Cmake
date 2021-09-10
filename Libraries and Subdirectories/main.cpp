001093e16ad297639e082ac5ce4e38d88d34fab3
Nguyễn Huy Chương <74437696+medalnguyen@users.noreply.github.com>
2021-09-10 16:33:58 +0700

    Test 1

---
+++ Libraries and Subdirectories/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cout << add(5.4f,5.6f) << std::endl;
+    return 0;
+}
---
+++ multiple file/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+float add(float a, float b);
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cout << add(4.5f, 5.5f) << std::endl;
+    return 0;
+}
